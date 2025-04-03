1c2767062948272b4550be679d09b43cb2ddc29e
fahritech04 <fahricoding@gmail.com>
2025-04-04 06:36:02 +0800

    Membuat tampilan about

--- app/src/main/java/com/raihanfahrifi3009/assessment1mobpro/navigation/Screen.kt
+++ app/src/main/java/com/raihanfahrifi3009/assessment1mobpro/navigation/Screen.kt
@@ -2,4 +2,5 @@ package com.raihanfahrifi3009.assessment1mobpro.navigation
 
 sealed class Screen(val route: String) {
     data object Home: Screen("mainScreen")
+    data object About: Screen("aboutScreen")
 }
