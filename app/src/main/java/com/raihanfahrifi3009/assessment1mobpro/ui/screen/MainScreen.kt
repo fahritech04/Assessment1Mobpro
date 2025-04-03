55f884b8a48d91fd57c0809052a5b9bd57d5375f
fahritech04 <fahricoding@gmail.com>
2025-04-03 17:24:43 +0800

    Menambahkan tittle & CurrencyConverterScreen

--- app/src/main/java/com/raihanfahrifi3009/assessment1mobpro/ui/screen/MainScreen.kt
+++ app/src/main/java/com/raihanfahrifi3009/assessment1mobpro/ui/screen/MainScreen.kt
@@ -5,6 +5,9 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.MaterialTheme
@@ -13,9 +16,11 @@ import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
 import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.raihanfahrifi3009.assessment1mobpro.R
 import com.raihanfahrifi3009.assessment1mobpro.ui.theme.Assessment1MobproTheme
 
@@ -47,16 +52,23 @@ fun MainScreen(){
             )
         }
     ) { innerPadding ->
-        ScreenContent(Modifier.padding(innerPadding))
+        CurrencyConverterScreen(Modifier.padding(innerPadding))
     }
 }
 
 @Composable
-fun ScreenContent(modifier: Modifier = Modifier){
-    Text(
-        text = "Hello Android",
-        modifier = modifier
-    )
+fun CurrencyConverterScreen(modifier: Modifier = Modifier){
+    Column(
+        modifier = modifier.fillMaxSize().padding(16.dp),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Text(
+            text = stringResource(R.string.title),
+            style = MaterialTheme.typography.headlineMedium,
+            modifier = Modifier.padding(bottom = 24.dp)
+        )
+    }
 }
 
 @Preview(showBackground = true)
