a429dc96cdbad9fec21efc450637207c5cd0fde0
fahritech04 <fahricoding@gmail.com>
2025-05-08 16:36:04 +0700

    Membuat toggle menu list/grid

--- app/src/main/java/com/raihanfahrifi3009/assessment1mobpro/ui/screen/BankScreen.kt
+++ app/src/main/java/com/raihanfahrifi3009/assessment1mobpro/ui/screen/BankScreen.kt
@@ -26,9 +26,13 @@ import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextOverflow
@@ -46,6 +50,7 @@ import com.raihanfahrifi3009.assessment1mobpro.util.ViewModelFactory
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun BankScreen(navController: NavHostController){
+    var showList by remember { mutableStateOf(true) }
 
     Scaffold(
         topBar = {
@@ -65,7 +70,22 @@ fun BankScreen(navController: NavHostController){
                 colors = TopAppBarDefaults.mediumTopAppBarColors(
                     containerColor = MaterialTheme.colorScheme.primaryContainer,
                     titleContentColor = MaterialTheme.colorScheme.primary,
-                )
+                ),
+                actions = {
+                    IconButton(onClick = { showList = !showList }) {
+                        Icon(
+                            painter = painterResource(
+                                if (showList) R.drawable.baseline_grid_view_24
+                                else R.drawable.baseline_view_list_24
+                            ),
+                            contentDescription = stringResource(
+                                if (showList) R.string.grid
+                                else R.string.list
+                            ),
+                            tint = MaterialTheme.colorScheme.primary
+                        )
+                    }
+                }
             )
         },
         floatingActionButton = {
