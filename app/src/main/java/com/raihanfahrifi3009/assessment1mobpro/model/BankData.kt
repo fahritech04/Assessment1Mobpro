8c6e1085ce1b52e01b3d59ca7cabd07378b0eb22
fahritech04 <fahricoding@gmail.com>
2025-05-08 13:59:33 +0700

    - Menyiapkan data di ViewModel - Membuat tampilan list - Menangani keadaan jika list kosong - Membuat item dapat diklik

---
+++ app/src/main/java/com/raihanfahrifi3009/assessment1mobpro/model/BankData.kt
@@ -0,0 +1,8 @@
+package com.raihanfahrifi3009.assessment1mobpro.model
+
+data class BankData(
+    val id: Long,
+    val namabank: String,
+    val catatan: String,
+    val tanggal: String
+)
--- app/src/main/java/com/raihanfahrifi3009/assessment1mobpro/ui/screen/BankScreen.kt
+++ app/src/main/java/com/raihanfahrifi3009/assessment1mobpro/ui/screen/BankScreen.kt
@@ -1,10 +1,20 @@
 package com.raihanfahrifi3009.assessment1mobpro.ui.screen
 
 import android.content.res.Configuration
+import android.widget.Toast
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.automirrored.filled.ArrowBack
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.HorizontalDivider
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
@@ -13,13 +23,19 @@ import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
 import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.lifecycle.viewmodel.compose.viewModel
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.rememberNavController
 import com.raihanfahrifi3009.assessment1mobpro.R
+import com.raihanfahrifi3009.assessment1mobpro.model.BankData
 import com.raihanfahrifi3009.assessment1mobpro.ui.theme.Assessment1MobproTheme
 
 @OptIn(ExperimentalMaterial3Api::class)
@@ -46,11 +62,51 @@ fun BankScreen(navController: NavHostController){
                 )
             )
         }
-    ) { innerPadding ->
-        Text(
-            text = stringResource(R.string.copyright),
-            modifier = Modifier.padding(innerPadding).padding(16.dp)
-        )
+    ) { padding ->
+        ScreenContent(Modifier.padding(padding))
+    }
+}
+
+@Composable
+fun ScreenContent(modifier: Modifier) {
+    val viewModel: BankViewModel = viewModel()
+    val data = viewModel.data
+    val context = LocalContext.current
+
+    if(data.isEmpty()){
+        Column (
+            modifier = modifier.fillMaxSize().padding(16.dp),
+            verticalArrangement = Arrangement.Center,
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+            Text(text = stringResource(id = R.string.list_kosong))
+        }
+    }
+    else {
+        LazyColumn (
+            modifier = modifier.fillMaxSize(),
+            contentPadding = PaddingValues(bottom = 84.dp)
+        ) {
+            items(data) {
+                ListItem(bankData = it){
+                    val pesan = context.getString(R.string.x_diklik, it.namabank)
+                    Toast.makeText(context, pesan, Toast.LENGTH_SHORT).show()
+                }
+                HorizontalDivider()
+            }
+        }
+    }
+}
+
+@Composable
+fun ListItem(bankData: BankData, onClick: () -> Unit){
+    Column (
+        modifier = Modifier.fillMaxWidth().clickable { onClick() }.padding(16.dp),
+        verticalArrangement = Arrangement.spacedBy(8.dp)
+    ) {
+        Text(text = bankData.namabank, maxLines = 1, overflow = TextOverflow.Ellipsis, fontWeight = FontWeight.Bold)
+        Text(text = bankData.catatan, maxLines = 1, overflow = TextOverflow.Ellipsis)
+        Text(text = bankData.tanggal)
     }
 }
 
